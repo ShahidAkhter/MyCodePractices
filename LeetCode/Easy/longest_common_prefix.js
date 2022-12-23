e6442c1842ddc6afdc2f317dadc35c98487668d8
Shahid Akhter <112084911+ShahidAkhter@users.noreply.github.com>
2022-12-23 14:03:33 +0530

    Update longest_common_prefix.js

--- LeetCode/Easy/longest_common_prefix.js
+++ LeetCode/Easy/longest_common_prefix.js
@@ -6,26 +6,22 @@ let longestCommonPrefix = (strs) => {
     let val = '';
     let bool = true;
     let Index = 0;
-    IndexArray=[]
+    // IndexArray = []
     for (let i = 0; i < strs.length; i++) {
         const strsItem = strs[i];
-        IndexArray.push(strsItem.length)
-    }
-    for (let i = 0; i < IndexArray.length; i++) {
-        const Values = IndexArray[i];
-        if (!IndexArray[i-1]) {
+        if (!strs[i - 1]) {
             continue;
         }
-        if (Values<IndexArray[i-1]) {
-            Index=Values;
-        }else{
-            Index=IndexArray[i-1];
+        if (strs[i].length < strs[i - 1].length) {
+            Index = strs[i].length;
+        } else {
+            Index = strs[i - 1].length;
         }
     }
     // console.log(Index)
     for (let i = 0; i < Index; i++) {
         for (let j = 0; j < strs.length; j++) {
-            if (!strs[j+1]||!strs[j+1][i]) {
+            if (!strs[j + 1] || !strs[j + 1][i]) {
                 continue;
             }
             if (strs[j][i] === strs[j + 1][i]) {
@@ -33,7 +29,7 @@ let longestCommonPrefix = (strs) => {
             } else {
                 bool = false;
             }
-            if (bool==false) {
+            if (bool == false) {
                 // console.log(val)
                 return val;
             }
@@ -42,7 +38,7 @@ let longestCommonPrefix = (strs) => {
         if (bool == true) {
             val += strs[i][i];
             // console.log(bool, val)
-        }else{
+        } else {
             // console.log(val)
             return val
         }
@@ -52,4 +48,4 @@ let longestCommonPrefix = (strs) => {
 };
 
 longestCommonPrefix(["flower", "flow", "flight"])
-longestCommonPrefix(["dog","racecar","car"])
+longestCommonPrefix(["dog", "racecar", "car"])
