7d6529b2153169d098da7eb48d9ae70482f6c758
Shahid Akhter <112084911+ShahidAkhter@users.noreply.github.com>
2022-12-23 14:24:17 +0530

    Update longest_common_prefix.js

--- LeetCode/Easy/longest_common_prefix.js
+++ LeetCode/Easy/longest_common_prefix.js
@@ -8,7 +8,7 @@ let longestCommonPrefix = (strs) => {
     let Index = 0;
     // IndexArray = []
     for (let i = 0; i < strs.length; i++) {
-        const strsItem = strs[i];
+        // const strsItem = strs[i];
         if (!strs[i - 1]) {
             continue;
         }
@@ -19,28 +19,35 @@ let longestCommonPrefix = (strs) => {
         }
     }
     // console.log(Index)
-    for (let i = 0; i < Index; i++) {
-        for (let j = 0; j < strs.length; j++) {
-            if (!strs[j + 1] || !strs[j + 1][i]) {
-                continue;
-            }
-            if (strs[j][i] === strs[j + 1][i]) {
-                bool = true;
-            } else {
-                bool = false;
-            }
-            if (bool == false) {
-                // console.log(val)
-                return val;
-            }
+    i=0;
+    j=0;
+    while (i<Index) {
+        if (!strs[j + 1] || !strs[j + 1][i]) {
+            continue;
         }
-        // console.log(bool)
-        if (bool == true) {
-            val += strs[i][i];
-            // console.log(bool, val)
+        if (strs[j][i] === strs[j + 1][i]) {
+            bool = true;
         } else {
+            bool = false;
+        }
+        // console.log(bool, val)
+
+        if (bool == false) {
             // console.log(val)
-            return val
+            return val;
+        }
+
+        j=j+1;
+        if (j >= (strs.length - 1)) {
+            j=0;
+            if (bool == true) {
+                val += strs[j][i];
+                // console.log(bool, val)
+            } else {
+                // console.log(val)
+                return val
+            }
+            i=i+1;
         }
     }
     // console.log(val)
@@ -49,3 +56,4 @@ let longestCommonPrefix = (strs) => {
 
 longestCommonPrefix(["flower", "flow", "flight"])
 longestCommonPrefix(["dog", "racecar", "car"])
+longestCommonPrefix(["goooooogledog", "goooooogleracecar", "gooooooglecar"])
